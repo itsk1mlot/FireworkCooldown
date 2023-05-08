071f24faa0c92fccc3c95ba339ca6f1f44c4956c
itsk1mlot <itskimlot@gmail.com>
2023-05-08 17:22:24 +0900

    🕒 쿨다운 전 2틱 대기 추가 / 👌 클릭감지

--- src/main/kotlin/io/github/itsk1mlot/rocketcool/listener/Interact.kt
+++ src/main/kotlin/io/github/itsk1mlot/rocketcool/listener/Interact.kt
@@ -3,6 +3,7 @@ package io.github.itsk1mlot.rocketcool.listener
 import io.github.itsk1mlot.rocketcool.Main
 import io.github.itsk1mlot.rocketcool.misc.Convert
 import net.kyori.adventure.text.Component
+import org.bukkit.Bukkit
 import org.bukkit.Material
 import org.bukkit.event.EventHandler
 import org.bukkit.event.Listener
@@ -13,12 +14,17 @@ class Interact: Listener {
     fun onInteract(event: PlayerInteractEvent) {
         val player = event.player
         val seconds = Main.instance.config.getInt("cooldown")
+        val ct = event.action
 
-        if (player.inventory.itemInMainHand.type == Material.FIREWORK_ROCKET || player.inventory.itemInOffHand.type == Material.FIREWORK_ROCKET) {
-            if (player.hasCooldown(Material.FIREWORK_ROCKET)) {
-                player.sendMessage(Component.text("§6[FireworkCooldown] §c아직 쿨다운이 ${Convert.toSeconds(player.getCooldown(Material.FIREWORK_ROCKET))}초 남았습니다!"))
-            } else {
-                player.setCooldown(Material.FIREWORK_ROCKET, Convert.toTick(seconds))
+        if (ct.isRightClick) {
+            if (player.inventory.itemInMainHand.type == Material.FIREWORK_ROCKET || player.inventory.itemInOffHand.type == Material.FIREWORK_ROCKET) {
+                if (player.hasCooldown(Material.FIREWORK_ROCKET)) {
+                    player.sendMessage(Component.text("§6[FireworkCooldown] §c아직 쿨다운이 ${Convert.toSeconds(player.getCooldown(Material.FIREWORK_ROCKET))}초 남았습니다!"))
+                } else {
+                    Bukkit.getScheduler().runTaskLater(Main.instance, Runnable {
+                        player.setCooldown(Material.FIREWORK_ROCKET, Convert.toTick(seconds))
+                    }, 2)
+                }
             }
         }
     }
